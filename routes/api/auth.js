c0f3cf75a651c632265abcbf8ff52a25b20996aa
Aayushi <aatulasyan143@gmail.com>
2020-08-28 20:59:22 +0530

    add user route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -77,10 +77,11 @@ router.post('/',
             
           );
           
-      } catch (error) {
-          
+      } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server error');          
       }
   }
+);
 
-
-)
+module.exports = router;
